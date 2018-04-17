6bee43b3235d7b7d977b610bd12fbc0cea4ad694
LIIGH-UNAM <34168459+LIIGH-UNAM@users.noreply.github.com>
2018-04-17 13:54:46 -0500

    Version 1.0 PMGL Pipeline

--- catAlignment_v1.0.pl
+++ catAlignment_v1.0.pl
@@ -151,7 +151,7 @@ sub main {
         $posGenomeAbsolutoLeft = ($posGenome - ($numberWalk*$numberNucleotides))-1;
     }
     else{
-        $posGenomeAbsolutoLeft = ($posGenome - (($numberWalk*$numberNucleotides))-1)+10;
+        $posGenomeAbsolutoLeft = ($posGenome - (($numberWalk*$numberNucleotides))-1)+ (10*($numberWalk-1));
     }
 
     my $posGenomeAbsolutoRight = $posGenome + ($kmerLength-1);
